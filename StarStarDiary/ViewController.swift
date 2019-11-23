641357639d5374e89dfefc89de4ab9e12dd9ec25
Caution Kim <caution.dev@gmail.com>
2019-11-23 15:57:31 +0900

    init project

---
+++ StarStarDiary/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StarStarDiary
+//
+//  Created by juhee on 2019/11/23.
+//  Copyright © 2019 mash-up. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
