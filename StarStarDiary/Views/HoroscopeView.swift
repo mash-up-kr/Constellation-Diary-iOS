598f4cef8f3c22fe129d32ba7c459f4d0e607269
부엉이 <o.o.wl.soft@gmail.com>
2019-11-24 01:24:29 +0900

    feat: HoroscopeView  UI Layout & Attribites 적용

--- StarStarDiary/Views/HoroscopeView.swift
+++ StarStarDiary/Views/HoroscopeView.swift
@@ -11,17 +11,16 @@ import SnapKit
 import Then
 
 class HoroscopeView: UIView {
-
+    
     // MARK: - UI
     private let titleLabel = UILabel()
     private let suggestionLabel = UILabel()
     private let fortuneItemsView = UIStackView()
-    private let detailTextView = UITextView()
-    private let completeButton = UIButton()
-
+    
     // MARK: - Methods
     override func draw(_ rect: CGRect) {
-        
+        setUpAttributes()
+        setUpConstraints()
     }
     
     // MARK: - Configure
@@ -30,4 +29,45 @@ class HoroscopeView: UIView {
 // MARK: - Layout & Attributes
 extension HoroscopeView {
     
+    private func setUpAttributes() {
+        
+        self.do {
+            $0.addSubview(titleLabel)
+            $0.addSubview(suggestionLabel)
+            $0.addSubview(fortuneItemsView)
+        }
+        
+        titleLabel.do {
+            $0.font.withSize(40)
+            $0.textColor = .black
+            $0.text = "별자리 운세"
+        }
+        
+        suggestionLabel.do {
+            $0.textColor = .gray122
+            $0.text = "행운의 소품을 확인하세요."
+        }
+        fortuneItemsView.do {
+            $0.backgroundColor = .black
+        }
+    }
+    
+    private func setUpConstraints() {
+        titleLabel.snp.makeConstraints { make in
+            make.top.equalTo(safeAreaLayoutGuide).inset(43)
+            make.centerX.equalToSuperview()
+        }
+        
+        suggestionLabel.snp.makeConstraints { make in
+            make.top.equalTo(titleLabel.snp.bottom).offset(4)
+            make.centerX.equalToSuperview()
+        }
+        
+        fortuneItemsView.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(suggestionLabel.snp.bottom).offset(32)
+            make.width.equalToSuperview().inset(90)
+            make.height.equalTo(70)
+        }
+    }
 }
