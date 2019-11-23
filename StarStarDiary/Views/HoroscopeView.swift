fb4d371a640516cb609e0632158d0d94885a36c8
부엉이 <o.o.wl.soft@gmail.com>
2019-11-23 23:58:01 +0900

    feat: UIComponent 프로퍼티 추가

---
+++ StarStarDiary/Views/HoroscopeView.swift
@@ -0,0 +1,33 @@
+//
+//  FortuneView.swift
+//  StarStarDiary
+//
+//  Created by 이동영 on 2019/11/23.
+//  Copyright © 2019 mash-up. All rights reserved.
+//
+
+import UIKit
+import SnapKit
+import Then
+
+class HoroscopeView: UIView {
+
+    // MARK: - UI
+    let titleLabel = UILabel()
+    let suggestionLabel = UILabel()
+    let fortuneItemsView = UIStackView()
+    let detailTextView = UITextView()
+    let completeButton = UIButton()
+
+    // MARK: - Methods
+    override func draw(_ rect: CGRect) {
+        
+    }
+    
+    // MARK: - Configure
+    
+}
+// MARK: - Layout & Attributes
+extension HoroscopeView {
+    
+}
