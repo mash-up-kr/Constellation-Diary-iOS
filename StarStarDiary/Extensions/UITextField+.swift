7c8730358ac5d82eee8bf531e95d814587ab9838
부엉이 <o.o.wl.soft@gmail.com>
2019-12-20 02:58:10 +0900

    feat: UITextField underline 메소드 추가 확장

--- StarStarDiary/Extensions/UITextField+.swift
+++ StarStarDiary/Extensions/UITextField+.swift
@@ -6,4 +6,20 @@
 //  Copyright © 2019 mash-up. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+extension UITextField {
+    func underlined(with color: UIColor) {
+        let border = CALayer()
+        let width = CGFloat(1.0)
+        
+        border.borderColor = color.cgColor
+        border.frame = CGRect(x: 0,
+                              y: self.bounds.size.height - width,
+                              width:  self.bounds.width,
+                              height: self.bounds.size.height)
+        border.borderWidth = width
+        self.layer.addSublayer(border)
+        self.layer.masksToBounds = true
+    }
+}
