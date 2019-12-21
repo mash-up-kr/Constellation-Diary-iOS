def7807af77c28e6e97a6fbbabe94c5661e53786
heyKaren <dlatngusch@gmail.com>
2019-12-22 02:25:05 +0900

    SettingsViewController 데이터 바인딩

--- StarStarDiary/ViewControllers/SettingsViewController.swift
+++ StarStarDiary/ViewControllers/SettingsViewController.swift
@@ -13,6 +13,10 @@ class SettingsViewController: UIViewController {
     @IBOutlet weak var navigationView: BaseNavigationView!
     @IBOutlet weak var tableView: UITableView!
     
+    // MARK: - Var
+    
+    private var items: SettingsViewItem!
+    
     // MARK: - Event
 
     @objc
@@ -26,14 +30,46 @@ class SettingsViewController: UIViewController {
         let leftTargetType: AddTargetType = (self, #selector(onClose(sender:)), .touchUpInside)
 
         navigationView.do {
+            $0.setBackgroundColor(color: .clear)
             $0.setBtnLeft(image: UIImage(named: "icClose24"), addTargetType: leftTargetType)
             $0.setTitle(title: "설정", titleColor: .black, image: nil)
+            $0.setBottomLine(isHidden: false)
         }
     }
     
     private func initTableView() {
-        tableView.delegate = self
-        tableView.dataSource = self
+        tableView.do {
+            $0.delegate = self
+            $0.dataSource = self
+            $0.backgroundColor = .clear
+            $0.register(BaseTableViewCell.self, forCellReuseIdentifier: "BaseTableViewCell")
+            $0.register(BaseTableViewHeaderFooterView.self,
+                        forHeaderFooterViewReuseIdentifier: "BaseTableViewHeaderFooterView")
+            $0.separatorStyle = .none
+            $0.bounces = false
+        }
+    }
+    
+    private func initVar() {
+        items = SettingsViewItem()
+        
+        for section in SectionMenu.allCases {
+            var cellItems: [SettingsViewCellItem] = []
+            
+            for cell in CellMenu.allCases where cell.sectionMenuType == section {
+                cellItems.append(SettingsViewCellItem(index: cell.rawValue,
+                                          title: cell.titleString,
+                                          subTitle: cell.subTitleString,
+                                          cellType: cell.cellType,
+                                          isSwitchOn: cell.isSwitchOn,
+                                          rightImage: cell.rightImage,
+                                          isHiddenBottomLine: cell.isHiddenBottomLine))
+            }
+            
+            items.sections.append(SectionItem(index: section.rawValue,
+                                              title: section.titleString,
+                                              cells: cellItems))
+        }
     }
 
     // MARK: - Life Cycle
@@ -42,6 +78,8 @@ class SettingsViewController: UIViewController {
         super.viewDidLoad()
 
         initNavigationView()
+        initTableView()
+        initVar()
     }
 
 }
@@ -49,13 +87,63 @@ class SettingsViewController: UIViewController {
 // MARK: - UITableViewDelegate, UITableViewDataSource
 
 extension SettingsViewController: UITableViewDelegate, UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return items.sections.count
+    }
+    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-         // TODO: - 아이템 count 으로 변경 예정
+        for sectionItem in items.sections where sectionItem.index == section {
+            return sectionItem.cells.count
+        }
+        
         return 0
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        // TODO: - 아이템의 cellType에 따라 변경 예정
-        return UITableViewCell()
+        let cell = tableView.dequeueReusableCell(withIdentifier: "BaseTableViewCell",
+                                                 for: indexPath) as! BaseTableViewCell
+        let cellItem = items.sections[indexPath.section].cells[indexPath.row]
+        cell.setEntity(cell: cellItem)
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        if let sectionMenuType = SectionMenu(rawValue: indexPath.section) {
+            switch sectionMenuType {
+            case .alarm: return 40.0
+            case .normal: return 64.0
+            }
+        }
+    
+        return 0.0
+    }
+    
+    // MARK: - HeaderFooter
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let sectionItem = items.sections[section]
+        let view = tableView.dequeueReusableHeaderFooterView(withIdentifier: "BaseTableViewHeaderFooterView") as! BaseTableViewHeaderFooterView
+        view.setEntity(title: sectionItem.title,
+                       titleColor: .gray122,
+                       font: UIFont.systemFont(ofSize: 12.0))
+        
+        return view
+    }
+    
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        if let sectionMenuType = SectionMenu(rawValue: section) {
+            switch sectionMenuType {
+            case .alarm: return 40.0
+            case .normal: return 8.0
+            }
+        }
+        
+        return 0.0
+    }
+    
+    // MARK: Event
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
     }
 }
