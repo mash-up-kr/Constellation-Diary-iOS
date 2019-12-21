c73db700d110a1f889e1dadd4b7ed81c97d2d6c9
heyKaren <dlatngusch@gmail.com>
2019-12-21 15:28:17 +0900

    SettingsViewController - view 관련 코드 추가

--- StarStarDiary/ViewControllers/SettingsViewController.swift
+++ StarStarDiary/ViewControllers/SettingsViewController.swift
@@ -11,6 +11,14 @@ import UIKit
 class SettingsViewController: UIViewController {
 
     @IBOutlet weak var navigationView: BaseNavigationView!
+    @IBOutlet weak var tableView: UITableView!
+    
+    // MARK: - Event
+
+    @objc
+    private func onClose(sender: AnyObject?) {
+        self.dismiss(animated: true, completion: nil)
+    }
 
     // MARK: - Init
 
@@ -18,9 +26,14 @@ class SettingsViewController: UIViewController {
         let leftTargetType: AddTargetType = (self, #selector(onClose(sender:)), .touchUpInside)
 
         navigationView.do {
-            $0.setBtnLeft(image: UIImage(named: "close"), addTargetType: leftTargetType)
+            $0.setBtnLeft(image: UIImage(named: "icClose24"), addTargetType: leftTargetType)
             $0.setTitle(title: "설정", titleColor: .black, image: nil)
         }
+    }
+    
+    private func initTableView() {
+        tableView.delegate = self
+        tableView.dataSource = self
     }
 
     // MARK: - Life Cycle
@@ -31,10 +44,18 @@ class SettingsViewController: UIViewController {
         initNavigationView()
     }
 
-    // MARK: - Event
+}
 
-    @objc
-    private func onClose(sender: AnyObject?) {
-        self.dismiss(animated: true, completion: nil)
+// MARK: - UITableViewDelegate, UITableViewDataSource
+
+extension SettingsViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+         // TODO: - 아이템 count 으로 변경 예정
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // TODO: - 아이템의 cellType에 따라 변경 예정
+        return UITableViewCell()
     }
 }
