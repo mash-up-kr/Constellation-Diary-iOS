a63e9ab252a1800853380de62cce4e6d18a9e5e6
부엉이 <o.o.wl.soft@gmail.com>
2020-01-12 04:13:52 +0900

    feat: ConstellationSelectionViewController 추가

---
+++ StarStarDiary/ViewControllers/ConstellationSelectionViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ConstellationSelectionViewController.swift
+//  StarStarDiary
+//
+//  Created by 이동영 on 2020/01/12.
+//  Copyright © 2020 mash-up. All rights reserved.
+//
+
+import UIKit
+
+class ConstellationSelectionViewController: UIViewController {
+    
+    // MARK: - UI
+    
+    let constellationCollectionView = UICollectionView(frame: .zero,
+                                                       collectionViewLayout: UICollectionViewFlowLayout())
+    let startButton = UIButton()
+    let messageLabel = UILabel()
+    
+}
+
+// MARK: - Layouts
+
+extension ConstellationSelectionViewController {
+     func setUpLayout() {
+        
+    }
+}
+
+// MARK: - Attributes
+
+extension ConstellationSelectionViewController {
+    func setUpAttribute() {
+        
+    }
+}
