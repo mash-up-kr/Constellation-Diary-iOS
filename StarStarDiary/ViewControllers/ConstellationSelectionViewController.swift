55224c74e589dfb004cc0c5c171dfe5da7d9155a
부엉이 <o.o.wl.soft@gmail.com>
2020-01-12 04:20:40 +0900

    feat: FlowLayoutDelegate 구현

--- StarStarDiary/ViewControllers/ConstellationSelectionViewController.swift
+++ StarStarDiary/ViewControllers/ConstellationSelectionViewController.swift
@@ -17,6 +17,11 @@ class ConstellationSelectionViewController: UIViewController {
     let startButton = UIButton()
     let messageLabel = UILabel()
     
+    // MARK: - Properties
+    private let boundary = UIScreen.main.bounds.width * 0.12
+    private let padding = UIScreen.main.bounds.width * 0.06
+    private let cardWidth = UIScreen.main.bounds.width * 0.64
+    
 }
 
 // MARK: - Layouts
@@ -32,5 +37,32 @@ extension ConstellationSelectionViewController {
 extension ConstellationSelectionViewController {
     func setUpAttribute() {
         
+    }
+}
+
+extension ConstellationSelectionViewController: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: cardWidth, height: collectionView.bounds.height)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return padding
+    }
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return padding
+    }
+    
+    func collectionView(_ collectionView: UICollectionView,
+                        layout collectionViewLayout: UICollectionViewLayout,
+                        insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 0, left: padding, bottom: 0, right: padding)
     }
 }
