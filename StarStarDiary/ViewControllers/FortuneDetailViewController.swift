f28d77e9c5963bb8cde55a6b78ed9aa264ca6307
Caution Kim <caution.dev@gmail.com>
2020-01-26 02:22:26 +0900

    feat: 운세 화면 디자인 적용

---
+++ StarStarDiary/ViewControllers/FortuneDetailViewController.swift
@@ -0,0 +1,108 @@
+//
+//  FortuneDetailViewController.swift
+//  StarStarDiary
+//
+//  Created by 이동영 on 2019/11/23.
+//  Copyright © 2019 mash-up. All rights reserved.
+//
+
+import UIKit
+import SnapKit
+import Then
+
+protocol FortuneDetailViewDelegate: class {
+    func didTapWriteDiary()
+}
+
+final class FortuneDetailViewController: UIViewController {
+    
+    weak var delegate: FortuneDetailViewDelegate?
+    
+    // MARK: - UI
+    
+    private let fortuneHeaderView = FortuneHeaderView(frame: .zero)
+    private let seperatorView = UIView()
+    private let detailLabel = UILabel()
+    private let completeButton = UIButton()
+    
+    // MARK: - Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setUpAttributes()
+        setUpConstraints()
+        // TOFO: - 실제 데이터 바인딩
+        fortuneHeaderView.bind(items: FortuneItem.allCases )
+    }
+    
+    // MARK: - Configure
+    
+    @objc private func writeDiary(_ sender: UIButton) {
+        delegate?.didTapWriteDiary()
+    }
+    
+}
+// MARK: - Layout & Attributes
+
+extension FortuneDetailViewController {
+    
+    private func setUpAttributes() {
+        view.do {
+            $0.backgroundColor = .white
+            $0.addSubview(fortuneHeaderView)
+            $0.addSubview(seperatorView)
+            $0.addSubview(detailLabel)
+            $0.addSubview(completeButton)
+        }
+        
+        seperatorView.do {
+            $0.backgroundColor = .white216
+        }
+        
+        detailLabel.do {
+            $0.numberOfLines = 0
+            $0.font = UIFont.systemFont(ofSize: 15, weight: .regular)
+            $0.textColor = .black
+            // FIXME: - 실제 데이터 모델과 바인드
+            $0.text = """
+            활기찬 한 주가 되겠네요. 한 주를 시작하는 월요일부터 기운이 넘치는 주간입니다. 유난히 친구를 많이 만나게 됩니다.
+            
+            즐거운 시간이 많겠지만 건강관리는 좀 하셔야 합니다. 특정한 목적을 가진 만남은 좋지 않습니다. 취미나 동호회활동은 약간 기대에 모자란 정도입니다.
+            """
+        }
+        
+        completeButton.do {
+            $0.backgroundColor = .navy3
+            $0.layer.cornerRadius = 5
+            $0.setTitle("일기 작성하기", for: .normal)
+            $0.addTarget(self, action: #selector(writeDiary(_:)), for: .touchUpInside)
+        }
+    }
+    
+    private func setUpConstraints() {
+        fortuneHeaderView.snp.makeConstraints {
+            $0.leading.trailing.top.equalToSuperview()
+            $0.bottom.equalTo(seperatorView.snp.top).offset(6)
+        }
+
+        seperatorView.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.leading.trailing.equalToSuperview().inset(32)
+            $0.height.equalTo(1)
+        }
+
+        detailLabel.snp.makeConstraints {
+            $0.top.equalTo(seperatorView.snp.bottom).offset(32)
+            $0.leading.trailing.equalToSuperview().inset(32)
+        }
+
+        completeButton.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.bottom.equalTo(view.safeAreaLayoutGuide).inset(20)
+            $0.leading.trailing.equalTo(view.safeAreaLayoutGuide).inset(20)
+            $0.height.equalTo(52)
+        }
+    }
+
+}
---
+++ StarStarDiary/Views/FortuneHeaderView.swift
@@ -0,0 +1,110 @@
+//
+//  FortuneHeaderView.swift
+//  StarStarDiary
+//
+//  Created by juhee on 2020/01/25.
+//  Copyright © 2020 mash-up. All rights reserved.
+//
+
+import UIKit
+
+
+final class FortuneHeaderView: UIView {
+    
+    var isDrawerHidden: Bool = false {
+        didSet {
+            self.drawerHandleView.isHidden = self.isDrawerHidden
+        }
+    }
+
+    private let drawerHandleView = UIView()
+    private let titleLabel = UILabel()
+    private let suggestionLabel = UILabel()
+    private let fortuneItemsView = UIStackView()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setUpAttributes()
+        setUpConstraints()
+    }
+
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+
+        setUpAttributes()
+        setUpConstraints()
+    }
+    
+    func bind(items: [FortuneItem]) {
+        items.forEach {
+            let itemView = FortuneItemView()
+            itemView.bind(item: $0)
+            fortuneItemsView.addArrangedSubview(itemView)
+        }
+    }
+    
+    private func setUpAttributes() {
+        self.do {
+            $0.backgroundColor = .white
+            $0.addSubview(drawerHandleView)
+            $0.addSubview(titleLabel)
+            $0.addSubview(suggestionLabel)
+            $0.addSubview(fortuneItemsView)
+        }
+        
+        drawerHandleView.do {
+            $0.backgroundColor = .black
+            $0.layer.cornerRadius = 1.5
+        }
+        
+        titleLabel.do {
+            $0.font = UIFont.systemFont(ofSize: 20, weight: .bold)
+            $0.textColor = .black
+            // TODO: - 실제 데이터 모델과 바인드
+            $0.text = "별자리 운세"
+        }
+        
+        suggestionLabel.do {
+            $0.font = UIFont.systemFont(ofSize: 13)
+            $0.textColor = .gray122
+            // TODO: - 실제 데이터 모델과 바인드
+            $0.text = "행운의 소품을 확인하세요."
+        }
+        
+        fortuneItemsView.do {
+            $0.backgroundColor = .black
+            $0.spacing = 4
+            $0.distribution = .fillEqually
+            $0.alignment = .center
+            $0.axis = .horizontal
+        }
+    }
+    
+    private func setUpConstraints() {
+        drawerHandleView.snp.makeConstraints {
+            $0.top.equalTo(safeAreaLayoutGuide).offset(10)
+            $0.width.equalTo(24)
+            $0.centerX.equalToSuperview()
+            $0.height.equalTo(3)
+        }
+        
+        titleLabel.snp.makeConstraints {
+            $0.top.equalTo(drawerHandleView.snp.bottom).offset(16)
+            $0.centerX.equalToSuperview()
+        }
+        
+        suggestionLabel.snp.makeConstraints {
+            $0.top.equalTo(titleLabel.snp.bottom).offset(1)
+            $0.centerX.equalToSuperview()
+        }
+        
+        fortuneItemsView.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.top.equalTo(suggestionLabel.snp.bottom).offset(16)
+            $0.height.equalTo(70)
+            $0.bottom.equalToSuperview().inset(23)
+        }
+    }
+
+}
