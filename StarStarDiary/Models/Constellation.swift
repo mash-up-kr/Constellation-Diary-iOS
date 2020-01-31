3b1d425c601a3b01c2ea5d04dff0a7c528d89766
부엉이 <o.o.wl.soft@gmail.com>
2020-01-31 21:15:26 +0900

    refactor: struct -> enum 으로 수정

--- StarStarDiary/Models/Constellation.swift
+++ StarStarDiary/Models/Constellation.swift
@@ -7,43 +7,52 @@
 //
 import UIKit
 
-struct Constellation {
-    let name: String
-    let date: String
-// TODO: - 추후 에셋 적용
-//    let icon: UIImage
-//    let image: UIImage
+enum Constellation: CaseIterable {
     
-    init(_ name: String, _ date: String) {
-        self.name = name
-        self.date = date
+    case  aries
+    case  taurus
+    case  gemini
+    case  cancer
+    case  leo
+    case  virgo
+    case  libra
+    case  scorpio
+    case  sagittarius
+    case  capricorn
+    case  aquarius
+    case  pisces
+    
+    var name: String {
+        switch self {
+        case .aries: return "양자리"
+        case .taurus: return "황소자리"
+        case .gemini: return "쌍둥이자리"
+        case .cancer: return "게자리"
+        case .leo: return "사자자리"
+        case .virgo: return "처녀자리"
+        case .libra: return "천칭자리"
+        case .scorpio: return "전갈자리"
+        case .sagittarius: return "궁수자리"
+        case .capricorn: return "염소자리"
+        case .aquarius: return "물병자리"
+        case .pisces: return "물고기 자리"
+        }
     }
-}
-
-extension Constellation {
-    static let aries = Constellation("양자리", "March 21 ~ April 19")
-    static let taurus = Constellation("황소자리", "April 20 ~ May 20")
-    static let gemini = Constellation("쌍둥이자리","May 21 ~June 21")
-    static let cancer = Constellation("게자리", "June 22 ~July 22")
-    static let leo = Constellation("사자자리", "July 23 ~ August 22")
-    static let virgo = Constellation("처녀자리", "August 23 ~ September 22")
-    static let libra = Constellation("천칭자리", "September 23 ~ October 23")
-    static let scorpio = Constellation("전갈자리", "October 24 ~ November 21")
-    static let sagittarius = Constellation("궁수자리", "November 22 ~ December 21")
-    static let capricorn = Constellation("염소자리", "December 22 ~ January 19")
-    static let aquarius = Constellation("물병자리", "January 20 ~ February 18")
-    static let pisces = Constellation("물고기 자리", "February 19 ~ March 20")
     
-    static let allCases = [aries,
-                           taurus,
-                           gemini,
-                           cancer,
-                           leo,
-                           virgo,
-                           libra,
-                           scorpio,
-                           sagittarius,
-                           capricorn,
-                           aquarius,
-                           pisces]
+    var date: String {
+        switch self {
+        case .aries: return "March 21 ~ April 19"
+        case .taurus: return "April 20 ~ May 20"
+        case .gemini: return "May 21 ~June 21"
+        case .cancer: return "June 22 ~July 22"
+        case .leo: return "July 23 ~ August 22"
+        case .virgo: return "August 23 ~ September 22"
+        case .libra : return "September 23 ~ October 23"
+        case .scorpio: return "October 24 ~ November 21"
+        case .sagittarius: return "November 22 ~ December 21"
+        case .capricorn: return "December 22 ~ January 19"
+        case .aquarius: return "January 20 ~ February 18"
+        case .pisces: return "February 19 ~ March 20"
+        }
+    }
 }
