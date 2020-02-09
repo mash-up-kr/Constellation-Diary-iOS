275ce2998e2830860163acc824edcf9442d6a093
부엉이 <o.o.wl.soft@gmail.com>
2020-02-09 20:46:37 +0900

    feat: AuthenticatedAPI TargetType 구현

--- StarStarDiary/Networks/Dto/Request/ReqModifyHoroscopeTimeDto.swift
+++ StarStarDiary/Networks/Dto/Request/ReqModifyHoroscopeTimeDto.swift
@@ -11,14 +11,3 @@ import Foundation
 struct ReqModifyHoroscopeTimeDto: Encodable {
     let date: String
 }
-//
-//protocol JSONObjectConvertible: Encodable {
-//    func asJSONObject() -> [String: String]
-//}
-//extension JSONObjectConvertible {
-//    func asJSONObject() -> [String: String] {
-//        let data = try? JSONEncoder().encode(self)
-//        return try! JSONSerialization.jsonObject(with: data!, options: .allowFragments) as! [String: String]
-//    }
-//}
-
