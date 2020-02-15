ffab1ae7f3465503add47f9eb8b89d7f384aa074
부엉이 <o.o.wl.soft@gmail.com>
2020-02-15 21:59:27 +0900

    refeator: Provider 분리

---
+++ StarStarDiary/Networks/Providers/NetworkProvider.swift
@@ -0,0 +1,78 @@
+//
+//  Networking.swift
+//  StarStarDiary
+//
+//  Created by 이동영 on 2020/02/09.
+//  Copyright © 2020 mash-up. All rights reserved.
+//
+
+import Foundation
+import Moya
+
+final class NetworkProvider: Provider {
+    var provider = MoyaProvider<API>()
+    
+    
+    func authenticationNumbersToFindPassword(email: String,
+                                             userId: String,
+                                             successHandler: @escaping () -> Void,
+                                             errorHandler: @escaping (Error) -> Void) {
+        request(.authenticationNumbersToFindPassword(email: email, userId: userId),
+                completion: successHandler,
+                failure: errorHandler)
+    }
+    
+    func authenticationToFindPassword(email: String,
+                                      number: Int,
+                                      userId: String,
+                                      successHandler: @escaping () -> Void,
+                                      errorHandler: @escaping (Error) -> Void) {
+        request(.authenticationToFindPassword(email: email, number: number, userId: userId),
+                completion: successHandler,
+                failure: errorHandler)
+    }
+    
+    func authenticationNumbersToSignUp(email: String,
+                                       successHandler: @escaping () -> Void,
+                                       errorHandler: @escaping (Error) -> Void) {
+        request(.authenticationNumbersToSignUp(email: email),
+                completion: successHandler,
+                failure: errorHandler)
+    }
+    
+    func authenticationToSignUp(email: String,
+                                number: Int,
+                                successHandler: @escaping () -> Void,
+                                errorHandler: @escaping (Error) -> Void) {
+        request(.authenticationToSignUp(email: email, number: number),
+                completion: successHandler,
+                failure: errorHandler)
+    }
+    
+    func checkId(id: String,
+                 successHandler: @escaping () -> Void,
+                 errorHandler: @escaping (Error) -> Void) {
+        request(.checkId(id: id),
+                completion: successHandler,
+                failure: errorHandler)
+    }
+    
+    func findId(email: String,
+                successHandler: @escaping () -> Void,
+                errorHandler: @escaping (Error) -> Void) {
+        request(.findId(email: email),
+                completion: successHandler,
+                failure: errorHandler)
+    }
+    
+    func signIn(fcmToken: String,
+                password: String,
+                userId: String,
+                successHandler: @escaping () -> Void,
+                errorHandler: @escaping (Error) -> Void) {
+        request(.signIn(fcmToken: fcmToken, password: password, userId: userId),
+                completion: successHandler,
+                failure: errorHandler)
+    }
+    
+}
